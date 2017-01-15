512e45a5495253eaa59b02a30b7a2cb5f3f92e95
Raja <rmnforever@gmail.com>
2017-01-15 22:57:38 +0530

    Add button to add doctor Helper functions in app.js to add doctors

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,32 @@
 app = angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+.run(function($ionicPlatform) 
+{
+  $ionicPlatform.ready(function() 
+  {
+    if(window.cordova && window.cordova.plugins.Keyboard) 
+    {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
     }
-    if(window.StatusBar) {
+    if(window.StatusBar) 
+    {
       StatusBar.styleDefault();
     }
   });
 })
 
-app.controller('DashboardController', function() {
-  this.items = [
+app.controller('DashboardController', function() 
+{
+  this.items = 
+  [
     { id: 1, name: "raja", speciality: "Cardio"},
     { id: 2, name: "mani", speciality: "Ortho"}
   ]
+
+  this.addDoctor = function()
+  {
+    newobj = { id: 4, name: "new", speciality: "Anaesthetist"}
+    this.items.push(newobj);
+  }
 });
