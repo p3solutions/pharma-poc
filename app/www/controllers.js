8ebd81e73c3f2c77f21695ec0612d948117733bc
Raja <rmnforever@gmail.com>
2017-01-29 22:52:25 +0530

    Make API request to get list of doctors in DB Allow CORS to make API calls from dev server

--- app/www/controllers.js
+++ app/www/controllers.js
@@ -3,7 +3,7 @@ angular.module('starter.controllers', [])
 .controller('AppCtrl', function($scope) {
 })
 
-.controller('PlaylistsCtrl', function($scope) {
+.controller('PlaylistsCtrl', function($scope, $http) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
     { title: 'Chill', id: 2 },
@@ -12,6 +12,18 @@ angular.module('starter.controllers', [])
     { title: 'Rap', id: 5 },
     { title: 'Cowbell', id: 6 }
   ];
+
+  $http.get("http://localhost:3000/doctors.json")
+            .success(function(data) {
+                $scope.firstname = data.firstname;
+                $scope.lastname = data.lastname;
+                console.log(data);
+            })
+            .error(function(data) {
+                alert("ERROR");
+            });
+
+  console.log("calling this function")
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
