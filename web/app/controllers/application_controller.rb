4ae64b474c93a04c8bf6769ffa6747e188dedeee
Raja <rmnforever@gmail.com>
2017-02-04 09:45:59 +0530

    Make changes to allow CORS

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
 
   def cors_set_access_control_headers
@@ -14,8 +15,9 @@ class ApplicationController < ActionController::Base
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-      headers['Access-Control-Max-Age'] = "1728000"
+      headers['Access-Control-Max-Age'] = '1728000'
 
       render :text => '', :content_type => 'text/plain'
     end
+  end
 end
