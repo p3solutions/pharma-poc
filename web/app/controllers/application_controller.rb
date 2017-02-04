4a2db57c16d7bcdcdf4c98530558c27627abb5e8
Raja <rmnforever@gmail.com>
2017-02-04 10:12:48 +0530

    Fix the damn CORS issues !!!!!

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
+  before_filter :cors_preflight_check
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
@@ -14,8 +13,9 @@ class ApplicationController < ActionController::Base
     if request.method == 'OPTIONS'
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-      headers['Access-Control-Max-Age'] = '1728000'
+      headers['Access-Control-Max-Age'] = "1728000"
 
       render :text => '', :content_type => 'text/plain'
     end
